5338c3d63baeb82c10a4d655e0dbfa52b0fe2449
Alyne MR Soares <35814001+AlyneSoares@users.noreply.github.com>
2018-06-14 13:47:22 -0300

    Simgle file in Javascript to be run with testcafe

---
+++ clickBanner.test.js
@@ -0,0 +1,10 @@
+"@fixture Cada clique no banner ajuda a manter o site";
+"@page http://cancerdemama.com.br";
+
+'@test'['Click no banner'] = {
+    '1.Clicar no banner':function () {
+        act.click('.img-responsive','');
+    },
+};
+
+
